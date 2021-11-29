26f503609ce4445c67c1a8784b21500ec3d5a679
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 11:25:08 -0800

    initialize

---
+++ OrderTest.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+//  ////////////////////////////////////////////////////////////////
+//  ORDER TEST
+//  By: Virgilio D. Cabading Jr.    Created at: November 29, 2021 1115
+//  ////////////////////////////////////////////////////////////////
+
+public class OrderTest {
+    public static void main(String[] args) {
+    
+        // Menu items
+    
+        // Order variables -- order1, order2 etc.
+    
+        // Application Simulations
+        // Use this example code to test various orders' updates
+        System.out.printf("Name: %s\n", order1.name);
+        System.out.printf("Total: %s\n", order1.total);
+        System.out.printf("Ready: %s\n", order1.ready);
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# Orders and Items Read Me
+
+This is a program for Coding Dojo > Java stack > Object Orientefd Programming.
+
+## In this program Virgilio Cabading will demonstrate ability to:
+1. Use another class file to help store data related to the app in a more organized way.
+2. Understand the value of grouping data together in a class to perform similar operations.
+3. Set member variables inside a class file
+4. Instantiate a new instance of a class (initialize a custom data type).
