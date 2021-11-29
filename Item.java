0bcf84390992e8c1dab592aa1162274c55a4fbab
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-11-29 13:01:06 -0800

    add item constructor, get, and set methods

--- Item.java
+++ Item.java
@@ -9,4 +9,37 @@ public class Item {
     double price;
 
     //  //// METHODS ///////////////////////////////////////////////
+
+    //  **** CONSTRUCTORS ******************************************
+    
+    public Item() {
+         this.name = "none";
+         this.price = 0;
+    }
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    //  **** SET METHODS *******************************************
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setPrice (double price) {
+        this.price = price;
+    }
+
+    //  **** GET METHODS *******************************************
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
 }
--- OrderTest.java
+++ OrderTest.java
@@ -7,8 +7,19 @@ import java.util.ArrayList;
 
 public class OrderTest {
     public static void main(String[] args) {
+        System.out.println("/////////////////////////////////////////////////////");
+        System.out.println("ORDER TEST APP\n");
     
         // Menu items
+        Item item1 = new Item("mocha", (double) 3);
+        Item item2 = new Item("latte", (double) 3.5);
+        Item item3 = new Item("drip coffee", (double) 1.5);
+        Item item4 = new Item("capuccino", (double) 3.75);
+        
+        System.out.printf("Item: %s | price: $%.2f\n", item1.getName(), item1.getPrice());
+        System.out.printf("Item: %s | price: $%.2f\n", item2.getName(), item2.getPrice());
+        System.out.printf("Item: %s | price: $%.2f\n", item3.getName(), item3.getPrice());
+        System.out.printf("Item: %s | price: $%.2f\n", item4.getName(), item4.getPrice());
     
         // Order variables -- order1, order2 etc.
     
